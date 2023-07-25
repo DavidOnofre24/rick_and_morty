72efb76eda5f254a20fe1b90abc10aa3cd1a3eca
David Onofre <davidonofre1010@gmail.com>
2023-07-24 22:27:35 -0500

    add characters screens

---
+++ lib/screens/characters_route.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty_app/bloc/cubit/characters_provider.dart';
+import 'package:rick_and_morty_app/navigation/navigation.dart';
+import 'package:rick_and_morty_app/screens/characters_screen.dart';
+
+class CharactersRoute extends RouteBuilder {
+  static String routeName = "/characters";
+
+  CharactersRoute(
+    BuildContext context, {
+    super.rootNavigator,
+  }) : super(context: context);
+
+  @override
+  Route getRoute() {
+    return MaterialPageRoute(
+      builder: (context) {
+        return MultiBlocProvider(
+          providers: [
+            charactersCubitProvider(),
+          ],
+          child: const CharactersScreen(),
+        );
+      },
+      settings: RouteSettings(name: routeName),
+    );
+  }
+}
---
+++ lib/screens/characters_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/cubit/characters_cubit.dart';
+
+class CharactersScreen extends StatelessWidget {
+  const CharactersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    context.read<CharactersCubit>();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Characters'),
+      ),
+      body: const Center(
+        child: Text('Hello World!'),
+      ),
+    );
+  }
+}
