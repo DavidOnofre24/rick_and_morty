ae971e68d7751cc03f9284b674538b8918900502
David Onofre <davidonofre1010@gmail.com>
2023-07-24 22:26:49 -0500

    add init app

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:rick_and_morty_app/dependecies_injection/dependencies_injection.dart';
+import 'package:rick_and_morty_app/screens/characters_route.dart';
 
-void main() {
+void main() async {
+  await setupDependencies();
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+final mainNavigationKey = GlobalKey<NavigatorState>();
+
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      navigatorKey: mainNavigationKey,
+      debugShowCheckedModeBanner: false,
+      title: 'Rick and morty app',
+      home: const InitialScreen(),
     );
+  }
+}
+
+class InitialScreen extends StatefulWidget {
+  const InitialScreen({Key? key}) : super(key: key);
+
+  @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      mainNavigationKey.currentState
+          ?.pushReplacement(CharactersRoute(context).getRoute());
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container());
   }
 }
