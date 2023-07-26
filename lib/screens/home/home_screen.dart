f9ce808e91c743d8be87c75872848d5bc5d014a7
David Onofre <davidonofre1010@gmail.com>
2023-07-26 15:03:06 -0500

    add backgroundcolor

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -28,6 +28,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: _widgetOptions.elementAt(_selectedIndex),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
